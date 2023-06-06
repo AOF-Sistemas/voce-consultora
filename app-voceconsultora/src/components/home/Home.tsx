2fdce2764a86af1f484da38b8d88124ee0b668ac
AOF-sistemas <aofsistemasltda@gmail.com>
2023-06-06 08:37:25 -0300

    ajuste inicial

---
+++ app-voceconsultora/src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledContainer, StyledText } from './styles';
+
+const Home = () => {
+  return (
+    <StyledContainer>
+      <StyledText>Teste</StyledText>
+    </StyledContainer>
+  );
+};
+export default Home;
